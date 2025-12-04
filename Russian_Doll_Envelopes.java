0f1c572d0bb0fbe095f52e68a81e666487c80da2
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2025-12-04 22:48:24 +0530

    Update Russian_Doll_Envelopes.java

--- Russian_Doll_Envelopes.java
+++ Russian_Doll_Envelopes.java
@@ -63,3 +63,30 @@ class Solution {
 
 ---------------------------------------------------------------------------------------------------
 
+class Solution {
+    public int maxEnvelopes(int[][] arr) {
+        Arrays.sort(arr,(a,b) -> {
+            if (a[0]==b[0]) 
+                return b[1]-a[1];
+            return a[0]-b[0];
+        });
+        int n=arr.length;
+        int[] dp=new int[n];
+        int size=0;
+        for(int i=0;i<n;i++){
+            int idx=binary(dp,size,arr[i][1]);
+            dp[idx]=arr[i][1];
+            if(idx==size) size++;
+        }
+        return size;
+    }
+    public int binary(int[] dp,int size,int n){
+        int l=0,h=size;
+        while(l<h){
+            int m=l+(h-l)/2;
+            if(dp[m]<n) l=m+1;
+            else h=m;
+        }
+        return l;
+    }
+}
