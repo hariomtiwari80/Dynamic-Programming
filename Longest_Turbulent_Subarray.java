ad9e66da663161711f30f86a8cb89c2c6b427ce4
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-01-21 19:20:14 +0530

    Update Longest_Turbulent_Subarray.java

--- Longest_Turbulent_Subarray.java
+++ Longest_Turbulent_Subarray.java
@@ -21,3 +21,38 @@ class Solution {
         return ans;
     }
 }
+--------------------------------------------------------------
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        int n=arr.length;
+        if(n<=1) return n;
+        int ans=1;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                boolean b=true;
+                int k=i;
+                while(k<n-1){
+                    if(arr[k]==arr[k+1]) break;
+                    if(b && arr[k]<arr[k+1]) break;
+                    if(!b && arr[k]>arr[k+1]) break;
+                    b=!b;
+                    k++;
+                }
+                ans=Math.max(ans,k-i+1);
+            }
+            else if(arr[i]<arr[i+1]){
+                boolean b=true;
+                int k=i;
+                while(k<n-1){
+                    if(arr[k]==arr[k+1]) break;
+                    if(b && arr[k]>arr[k+1]) break;
+                    if(!b && arr[k]<arr[k+1]) break;
+                    b=!b;
+                    k++;
+                }
+                ans=Math.max(ans,k-i+1);
+            }
+        }
+        return ans;
+    }
+}
